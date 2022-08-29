c88788a6a51a70d1cb7a0fef21a12bc68ddfd3ec
jabru <tiritoglumert@gmail.com>
2022-08-29 20:25:37 +0300

    Update

--- C++/Ders/Ders-5/main.cpp
+++ C++/Ders/Ders-5/main.cpp
@@ -7,16 +7,17 @@ using namespace std;
 
 int main() {
 
-    Employe Ali = *new Employe("Ali","Veli",2);
-    Engineer Ahmet = *new Engineer("Ahmet","Mehmet",20,"A");
-    Technician Vural = *new Technician("Vural","Babacan",20,55);
-    Technician Birol = *new Technician("Birol","Agca",20,65);
+//    Employe Ali = *new Employe("Ali", "Veli", 2);
+//    Engineer Ahmet = *new Engineer("Ahmet", "Mehmet", 20, "A");
+//    Technician Vural = *new Technician("Vural", "Babacan", 20, 55);
+//    Technician Birol = *new Technician("Birol", "Agca", 20, 65);
 
-    cout << Ali << endl;
-    cout << Ahmet << endl;
-    cout << Vural << endl;
-    cout << Birol << endl;
+//    cout << Ali << endl;
+//    cout << Ahmet << endl;
+//    cout << Vural << endl;
+//    cout << Birol << endl;
 
+//    cout << (Vural < Birol) << endl;
 
     return 0;
 }
---
+++ C++/Ders/untitled/Calculator.h
@@ -0,0 +1,14 @@
+//
+// Created by jabru on 8/29/2022.
+//
+
+#ifndef UNTITLED_CALCULATOR_H
+#define UNTITLED_CALCULATOR_H
+
+
+class Calculator {
+
+};
+
+
+#endif //UNTITLED_CALCULATOR_H
