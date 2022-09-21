459ad076b213e960de67e9891180a05a962cc48b
jabru <tiritoglumert@gmail.com>
2022-09-21 14:04:17 +0300

    Update!

--- JAVA/Ders/Sorting & Searching/Bubble Sort/src/Main.java
+++ JAVA/Ders/Sorting & Searching/Bubble Sort/src/Main.java
@@ -6,32 +6,88 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("Comparations.txt");
+//        File file = new File("Comparations.txt");
+//
+//        int digit = 1;
+//        for (int i = 0; i < 7; i++, digit *= 10) {
+//            int[] randomArray = generateRandomArray(100 * digit, 1000000);
+//
+//            Date start = new Date();
+////            bubbleSort(randomArray);
+////            selectionSort(randomArray);
+////            insertionSort(randomArray);
+////            mergeSort(randomArray,0,randomArray.length-1);
+////            quickSort(randomArray,0,randomArray.length-1);
+//
+//            Date end = new Date();
+//
+//            try {
+//                FileWriter fileWriter = new FileWriter(file, true);
+//                fileWriter.write("Sorting " + 100 * digit + " Elements: " + (end.getTime() - start.getTime()) + "ms\n");
+//                fileWriter.close();
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
 
-        int digit = 1;
-        for(int i = 0; i<4; i++, digit*=10){
-            int[] randomArray = generateRandomArray(100*digit,100000); // digit ?
+//        contains();
 
-            Date start = new Date();
-//            bubbleSort(randomArray);
-//            selectionSort(randomArray);
-            insertionSort2(randomArray);
-            Date end = new Date();
+        int[] arr = generateRandomArray(100000000,1000000);
+        quickSort(arr,0,arr.length-1);
+        arr[100000000-1] = 10000000;
 
-            try {
-                FileWriter fileWriter = new FileWriter(file,true);
-                fileWriter.write("Sorting " + 100*digit + " Elements Using Bubble Sort: " + (end.getTime() - start.getTime()) + "ms\n");
-                fileWriter.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        Date start = new Date();
+        binarySearch(arr,10000000,0,arr.length-1);
+        Date end = new Date();
+
+        System.out.println(90000000 + " found in " + (end.getTime()-start.getTime()) + "ms");
+    }
+
+    static void contains(){
+        int[] randomArray = generateRandomArray(10000000, 1000000);
+        quickSort(randomArray,0,randomArray.length-1);
+
+        randomArray[9000000] = 9000000;
+        int target = 9000000;
+        Date start = new Date();
+
+        for (int i = 0; i < randomArray.length; i++){
+            if (randomArray[i] == target){
+                Date end = new Date();
+                System.out.println(target + " found in " + (end.getTime()-start.getTime()) + "ms");
+                break;
             }
+        }
+
+
+        System.out.println();
+    }
+
+    static void binarySearch(int[] array, int x, int low, int high) {
+
+        // Repeat until the pointers low and high meet each other
+        int count = 0;
+        while (low <= high) {
+            count++;
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] == x){
+                System.out.println("COUNT = " + count);
+                return;
+            }
+
+            if (array[mid] < x)
+                low = mid + 1;
+
+            else
+                high = mid - 1;
         }
     }
 
-    static void bubbleSort(int[] array){
-        for (int i = 0; i < array.length-1; i++){ // hangi kod veya sistemde +1 kullanılabilirdi ?
+    static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) { // hangi kod veya sistemde +1 kullanılabilirdi ?
             boolean ok = false;
-            for (int j = 0; j < array.length-i-1; j++) { // j aşağıda i yukarda mı olmak zorunda ? kod asıl kodu içine mi alıcak hep, dışarıdan sayıcak ??
+            for (int j = 0; j < array.length - i - 1; j++) { // j aşağıda i yukarda mı olmak zorunda ? kod asıl kodu içine mi alıcak hep, dışarıdan sayıcak ??
                 if (array[j + 1] < array[j]) { // j öncü ve asıl kod içeride ve daha derinde ve geriden gelen kod i yukarıda, gerçi doğrusu da böyle gibi
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
@@ -39,17 +95,17 @@ public class Main {
                     ok = true;
                 }
             }
-            if(!ok)
+            if (!ok)
                 break;
         }
     }
 
-    static void selectionSort(int[] array){
-        for(int step = 0; step < array.length-1; step++){
+    static void selectionSort(int[] array) {
+        for (int step = 0; step < array.length - 1; step++) {
             int min_idx = step;
 
-            for(int i = step+1; i<array.length; i++){
-                if(array[i] < array[min_idx])
+            for (int i = step + 1; i < array.length; i++) {
+                if (array[i] < array[min_idx])
                     min_idx = i;
             }
 
@@ -59,25 +115,101 @@ public class Main {
         }
     }
 
-    static void insertionSort(int[] array){
-        for(int step = 1; step <array.length; step++){
+    static void insertionSort(int[] array) {
+        for (int step = 1; step < array.length; step++) {
             int key = array[step];
-            int j = step-1;
+            int j = step - 1;
 
-            while(j >= 0 && key < array[j]){
-                array[j+1] = array[j];
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
                 j--;
             }
 
-            array[j+1] = key;
+            array[j + 1] = key;
         }
     }
 
-    static int[] generateRandomArray(int size, int bound){
+    static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m; // buraya isaret
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    static int partition(int[] arr, int start, int end){
+        int pivot = arr[end];
+        int i = (start-1); // -1 ?
+
+        for(int j = start; j<end; j++){
+            if(arr[j] <= pivot){
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = temp;
+
+        return i+1;
+    }
+
+    static void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            int pi = partition(arr,low,high);
+            quickSort(arr,low,pi-1);
+            quickSort(arr, pi+1, high);
+        }
+    }
+
+    static int[] generateRandomArray(int size, int bound) {
         int[] ans = new int[size];
         Random random = new Random();
 
-        for(int i = 0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             ans[i] = random.nextInt(bound);
         }
         return ans;
--- JAVA/Ders/Sorting & Searching/Bubble Sort/src/Test.java
+++ JAVA/Ders/Sorting & Searching/Bubble Sort/src/Test.java
@@ -2,14 +2,15 @@ import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr = generateRandomArray(5,100);
-        insertionSort(arr);
+        int[] arr = generateRandomArray(100, 100);
+        mergeSort(arr,0,arr.length-1);
+        System.out.println("x");
     }
 
-    static void bubble(int[] array){
-        for (int i = 0; i < array.length-1; i++){
+    static void bubble(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
             boolean ok = false;
-            for (int j = 0; j < array.length-i-1; j++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
                 if (array[j + 1] < array[j]) {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
@@ -17,26 +18,26 @@ public class Test {
                     ok = true;
                 }
             }
-            if(!ok)
+            if (!ok)
                 break;
         }
     }
 
-    static int[] generateRandomArray(int size, int bound){
+    static int[] generateRandomArray(int size, int bound) {
         int[] ans = new int[size];
         Random random = new Random();
 
-        for(int i = 0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             ans[i] = random.nextInt(bound);
         }
         return ans;
     }
 
-    static void selectionSort(int[] array){
-        for (int i = 0; i < array.length-1; i++){
+    static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
             int min = i;
-            for (int j = i+1; j < array.length; j++){
-                if (array[j] < array[min]){
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
                     min = j;
                 }
             }
@@ -47,15 +48,89 @@ public class Test {
         }
     }
 
-    static void insertionSort(int[] array){
-        for (int i = 1; i < array.length; i++){
+    static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
             int key = array[i];
-            int j = i-1;
-            while (j >= 0 && array[j] > key){
-                array[j+1] = array[j];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
                 --j;
             }
-            array[j+1] = key;
+            array[j + 1] = key;
         }
+    }
+
+    static void mergeSort(int[] array, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(array, l, m);
+            mergeSort(array, m + 1, r);
+            merge(array, l, m, r);
+        }
+    }
+
+    private static void merge(int[] array, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] M = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = array[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            M[j] = array[m + 1 + j];
+        }
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2){
+            if (L[i] < M[j]){
+                array[k] = L[i];
+                i++;
+            }else{
+                array[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (j < n2){
+            array[k] = M[j];
+            j++;
+            k++;
+        }
+        while (i < n1){
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+    }
+
+    private static void quickSort(int[] array, int start, int end){
+        if (end < start){
+            int pivot = partition(array, start, end);
+            quickSort(array, start, pivot-1);
+            quickSort(array, pivot+1, end);
+        }
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int i = start-1;
+
+        for (int j = start; j < end; j++){
+            if (array[j] <= pivot){
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp2 = array[i+1];
+        array[i+1] = array[end];
+        array[end] = temp2;
+
+        return i+1;
     }
 }
